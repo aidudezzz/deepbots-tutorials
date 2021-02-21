aa42d7dc2c682fedf4a9fe62ada9286b2c219c55
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2021-02-21 12:24:26 +0200

    Renamed controller dir

--- robotSupervisorSchemeTutorial/full_project/controllers/robotSupervisorController/robotSupervisorController.py
+++ robotSupervisorSchemeTutorial/full_project/controllers/robotSupervisorController/robotSupervisorController.py
@@ -1,11 +1,13 @@
 from deepbots.supervisor.controllers.robot_supervisor import RobotSupervisor
-from utilities import normalizeToRange
+
+from utilities import normalizeToRange, plotData
+from agent.PPO_agent import PPOAgent, Transition
 
 from gym.spaces import Box, Discrete
 import numpy as np
 
 
-class CartPoleRobotSupervisor(RobotSupervisor):
+class CartPoleRobot(RobotSupervisor):
     """
     CartPoleRobotSupervisor acts as an environment having all the appropriate methods such as get_reward().
     This class utilizes the robot-supervisor scheme combining both the robot controls and the environment
@@ -201,3 +203,76 @@ class CartPoleRobotSupervisor(RobotSupervisor):
         :return:
         """
         print("render() is not used")
+
+
+# Initialize supervisor object
+env = CartPoleRobotSupervisor()
+
+
+# The agent used here is trained with the PPO algorithm (https://arxiv.org/abs/1707.06347).
+# We pass 4 as numberOfInputs and 2 as numberOfOutputs, taken from the gym spaces
+agent = PPOAgent(env.observation_space.shape[0], env.action_space.n)
+
+solved = False  # Whether the solved requirement is met
+
+episodeCount = 0
+episodeLimit = 2000
+averageEpisodeActionProbs = []  # Save average episode taken actions probability to plot later
+
+# Run outer loop until the episodes limit is reached or the task is solved
+while not solved and episodeCount < episodeLimit:
+    state = env.reset()  # Reset robot and get starting observation
+    env.episodeScore = 0
+    actionProbs = []  # This list holds the probability of each chosen action
+
+    # Inner loop is the episode loop
+    for step in range(env.stepsPerEpisode):
+        # In training mode the agent samples from the probability distribution, naturally implementing exploration
+        selectedAction, actionProb = agent.work(state, type_="selectAction")
+        # Save the current selectedAction's probability
+        actionProbs.append(actionProb)
+
+        # Step the supervisor to get the current selectedAction reward, the new state and whether we reached the
+        # done condition
+        newState, reward, done, info = env.step([selectedAction])
+
+        # Save the current state transition in agent's memory
+        trans = Transition(state, selectedAction, actionProb, reward, newState)
+        agent.storeTransition(trans)
+
+        env.episodeScore += reward  # Accumulate episode reward
+        if done:
+            # Save the episode's score
+            env.episodeScoreList.append(env.episodeScore)
+            agent.trainStep(batchSize=step + 1)
+            solved = env.solved()  # Check whether the task is solved
+            break
+
+        state = newState  # state for next step is current step's newState
+
+    print("Episode #", episodeCount, "score:", env.episodeScore)
+    # The average action probability tells us how confident the agent was of its actions.
+    # By looking at this we can check whether the agent is converging to a certain policy.
+    avgActionProb = mean(actionProbs)
+    averageEpisodeActionProbs.append(avgActionProb)
+    print("Avg action prob:", avgActionProb)
+
+    episodeCount += 1  # Increment episode counter
+
+
+if not solved:
+    print("Reached episode limit and task was not solved, deploying agent for testing...")
+else:
+    print("Task is solved, deploying agent for testing...")
+
+state = env.reset()
+env.episodeScore = 0
+while True:
+    selectedAction, actionProb = agent.work(state, type_="selectActionMax")
+    state, reward, done, _ = env.step([selectedAction])
+    env.episodeScore += reward  # Accumulate episode reward
+
+    if done:
+        print("Reward accumulated =", env.episodeScore)
+        env.episodeScore = 0
+        state = env.reset()
