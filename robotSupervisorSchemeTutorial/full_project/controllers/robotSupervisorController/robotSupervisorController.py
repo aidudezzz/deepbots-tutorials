67717f54240b4793169eed07d1537ecad71de49d
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2021-02-23 19:24:47 +0200

    Updated complete example

--- robotSupervisorSchemeTutorial/full_project/controllers/robotSupervisorController/robotSupervisorController.py
+++ robotSupervisorSchemeTutorial/full_project/controllers/robotSupervisorController/robotSupervisorController.py
@@ -1,94 +1,35 @@
 from deepbots.supervisor.controllers.robot_supervisor import RobotSupervisor
-
 from utilities import normalizeToRange, plotData
-from agent.PPO_agent import PPOAgent, Transition
+from PPO_agent import PPOAgent, Transition
 
 from gym.spaces import Box, Discrete
 import numpy as np
 
 
-class CartPoleRobot(RobotSupervisor):
-    """
-    CartPoleRobotSupervisor acts as an environment having all the appropriate methods such as get_reward().
-    This class utilizes the robot-supervisor scheme combining both the robot controls and the environment
-    in the same class. Moreover, the reset procedure used is the default implemented reset.
-    This class is made with the new release of deepbots in mind that fully integrates gym.Env, using gym.spaces.
-
-    Taken from https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py and modified
-    for Webots.
-    Description:
-        A pole is attached by an un-actuated joint to a cart, which moves forwards and backwards. The pendulum
-        starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's
-        velocity.
-    Source:
-        This environment corresponds to the version of the cart-pole problem described
-        by Barto, Sutton, and Anderson
-    Observation:
-        Type: Box(4)
-        Num	Observation                 Min         Max
-        0	Cart Position z axis      -0.4            0.4
-        1	Cart Velocity             -Inf            Inf
-        2	Pole Angle                -1.3 rad        1.3 rad
-        3	Pole Velocity At Tip      -Inf            Inf
-
-    Actions:
-        Type: Discrete(2)
-        Num	Action
-        0	Move cart forward
-        1	Move cart backward
-
-        Note: The amount the velocity that is reduced or increased is not fixed; it depends on the angle the pole is
-        pointing. This is because the center of gravity of the pole increases the amount of energy needed to move the
-        cart underneath it
-    Reward:
-        Reward is 1 for every step taken, including the termination step
-    Starting State:
-        [0.0, 0.0, 0.0, 0.0]
-    Episode Termination:
-        Pole Angle is more than 0.261799388 rad (15 degrees)
-        Cart Position is more than 0.39 on z axis (cart has reached arena edge)
-        Episode length is greater than 200
-        Solved Requirements (average episode score in last 100 episodes > 195.0)
-    """
-
+class CartpoleRobot(RobotSupervisor):
     def __init__(self):
-        """
-        In the constructor the observation_space and action_space are set and references to the various components
-        of the robot required are initialized here.
-        """
-
         super().__init__()
-
-        # Set up gym spaces
         self.observation_space = Box(low=np.array([-0.4, -np.inf, -1.3, -np.inf]),
                                      high=np.array([0.4, np.inf, 1.3, np.inf]),
                                      dtype=np.float64)
         self.action_space = Discrete(2)
 
-        # Set up various robot components
         self.robot = self.getSelf()  # Grab the robot reference from the supervisor to access various robot methods
         self.positionSensor = self.getDevice("polePosSensor")
         self.positionSensor.enable(self.timestep)
 
         self.poleEndpoint = self.getFromDef("POLE_ENDPOINT")
-
-        self.wheels = [None for _ in range(4)]
-        self.setup_motors()
-
-        # Set up misc
-        self.stepsPerEpisode = 200  # How many steps to run each episode (changing this messes up the solved condition)
+        self.wheels = []
+        for wheelName in ['wheel1', 'wheel2', 'wheel3', 'wheel4']:
+            wheel = self.getDevice(wheelName)  # Get the wheel handle
+            wheel.setPosition(float('inf'))  # Set starting position
+            wheel.setVelocity(0.0)  # Zero out starting velocity
+            self.wheels.append(wheel)
+        self.stepsPerEpisode = 200  # Max number of steps per episode
         self.episodeScore = 0  # Score accumulated during an episode
         self.episodeScoreList = []  # A list to save all the episode scores, used to check if task is solved
 
     def get_observations(self):
-        """
-        This get_observation implementation builds the required observation for the CartPole problem.
-        All values apart are gathered here from the robot and poleEndpoint objects.
-        All values are normalized appropriately to [-1, 1], according to their original ranges.
-
-        :return: Observation: [cartPosition, cartVelocity, poleAngle, poleTipVelocity]
-        :rtype: list
-        """
         # Position on z axis
         cartPosition = normalizeToRange(self.robot.getPosition()[2], -0.4, 0.4, -1.0, 1.0)
         # Linear velocity on z axis
@@ -100,25 +41,10 @@ class CartPoleRobot(RobotSupervisor):
 
         return [cartPosition, cartVelocity, poleAngle, endpointVelocity]
 
-    def get_reward(self, action):
-        """
-        Reward is +1 for each step taken, including the termination step.
-
-        :param action: Not used, defaults to None
-        :type action: None, optional
-        :return: Always 1
-        :rtype: int
-        """
+    def get_reward(self, action=None):
         return 1
 
     def is_done(self):
-        """
-        An episode is done if the score is over 195.0, or if the pole is off balance, or the cart position is on the
-        arena's edges.
-
-        :return: True if termination conditions are met, False otherwise
-        :rtype: bool
-        """
         if self.episodeScore > 195.0:
             return True
 
@@ -133,39 +59,16 @@ class CartPoleRobot(RobotSupervisor):
         return False
 
     def solved(self):
-        """
-        This method checks whether the CartPole task is solved, so training terminates.
-        Solved condition requires that the average episode score of last 100 episodes is over 195.0.
-
-        :return: True if task is solved, False otherwise
-        :rtype: bool
-        """
         if len(self.episodeScoreList) > 100:  # Over 100 trials thus far
-            if np.mean(self.episodeScoreList[-100:]) > 195.0:  # Last 100 episode scores average value
+            if np.mean(self.episodeScoreList[-100:]) > 195.0:  # Last 100 episodes' scores average value
                 return True
         return False
 
     def get_default_observation(self):
-        """
-        Simple implementation returning the default observation which is a zero vector in the shape
-        of the observation space.
-        :return: Starting observation zero vector
-        :rtype: list
-        """
         return [0.0 for _ in range(self.observation_space.shape[0])]
 
     def apply_action(self, action):
-        """
-        This method uses the action list provided, which contains the next action to be executed by the robot.
-        It contains an integer denoting the action, either 0 or 1, with 0 being forward and
-        1 being backward movement. The corresponding motorSpeed value is applied to the wheels.
-
-        :param action: The list that contains the action integer
-        :type action: list of int
-        """
         action = int(action[0])
-
-        assert action == 0 or action == 1, "CartPoleRobot controller got incorrect action value: " + str(action)
 
         if action == 0:
             motorSpeed = 5.0
@@ -176,103 +79,49 @@ class CartPoleRobot(RobotSupervisor):
             self.wheels[i].setPosition(float('inf'))
             self.wheels[i].setVelocity(motorSpeed)
 
-    def setup_motors(self):
-        """
-        This method initializes the four wheels, storing the references inside a list and setting the starting
-        positions and velocities.
-        """
-        self.wheels[0] = self.getDevice('wheel1')
-        self.wheels[1] = self.getDevice('wheel2')
-        self.wheels[2] = self.getDevice('wheel3')
-        self.wheels[3] = self.getDevice('wheel4')
-        for i in range(len(self.wheels)):
-            self.wheels[i].setPosition(float('inf'))
-            self.wheels[i].setVelocity(0.0)
-
-    def get_info(self):
-        """
-        Dummy implementation of get_info.
-        :return: Empty dict
-        """
-        return {}
-
     def render(self, mode='human'):
-        """
-        Dummy implementation of render
-        :param mode:
-        :return:
-        """
         print("render() is not used")
 
+    def get_info(self):
+        return None
 
-# Initialize supervisor object
-env = CartPoleRobotSupervisor()
 
-
-# The agent used here is trained with the PPO algorithm (https://arxiv.org/abs/1707.06347).
-# We pass 4 as numberOfInputs and 2 as numberOfOutputs, taken from the gym spaces
-agent = PPOAgent(env.observation_space.shape[0], env.action_space.n)
-
-solved = False  # Whether the solved requirement is met
-
+env = CartpoleRobot()
+agent = PPOAgent(numberOfInputs=env.observation_space.shape[0], numberOfActorOutputs=env.action_space.n)
+solved = False
 episodeCount = 0
 episodeLimit = 2000
-averageEpisodeActionProbs = []  # Save average episode taken actions probability to plot later
-
 # Run outer loop until the episodes limit is reached or the task is solved
 while not solved and episodeCount < episodeLimit:
-    state = env.reset()  # Reset robot and get starting observation
+    observation = env.reset()  # Reset robot and get starting observation
     env.episodeScore = 0
-    actionProbs = []  # This list holds the probability of each chosen action
-
-    # Inner loop is the episode loop
     for step in range(env.stepsPerEpisode):
         # In training mode the agent samples from the probability distribution, naturally implementing exploration
-        selectedAction, actionProb = agent.work(state, type_="selectAction")
-        # Save the current selectedAction's probability
-        actionProbs.append(actionProb)
-
-        # Step the supervisor to get the current selectedAction reward, the new state and whether we reached the
-        # done condition
-        newState, reward, done, info = env.step([selectedAction])
+        selectedAction, actionProb = agent.work(observation, type_="selectAction")
+        # Step the supervisor to get the current selectedAction's reward, the new observation and whether we reached
+        # the done condition
+        newObservation, reward, done, info = env.step([selectedAction])
 
         # Save the current state transition in agent's memory
-        trans = Transition(state, selectedAction, actionProb, reward, newState)
+        trans = Transition(observation, selectedAction, actionProb, reward, newObservation)
         agent.storeTransition(trans)
-
-        env.episodeScore += reward  # Accumulate episode reward
         if done:
             # Save the episode's score
             env.episodeScoreList.append(env.episodeScore)
-            agent.trainStep(batchSize=step + 1)
+            agent.trainStep(batchSize=step)
             solved = env.solved()  # Check whether the task is solved
             break
 
-        state = newState  # state for next step is current step's newState
-
+        env.episodeScore += reward  # Accumulate episode reward
+        observation = newObservation  # observation for next step is current step's newObservation
     print("Episode #", episodeCount, "score:", env.episodeScore)
-    # The average action probability tells us how confident the agent was of its actions.
-    # By looking at this we can check whether the agent is converging to a certain policy.
-    avgActionProb = mean(actionProbs)
-    averageEpisodeActionProbs.append(avgActionProb)
-    print("Avg action prob:", avgActionProb)
-
     episodeCount += 1  # Increment episode counter
 
-
 if not solved:
-    print("Reached episode limit and task was not solved, deploying agent for testing...")
-else:
+    print("Task is not solved, deploying agent for testing...")
+elif solved:
     print("Task is solved, deploying agent for testing...")
-
-state = env.reset()
-env.episodeScore = 0
+observation = env.reset()
 while True:
-    selectedAction, actionProb = agent.work(state, type_="selectActionMax")
-    state, reward, done, _ = env.step([selectedAction])
-    env.episodeScore += reward  # Accumulate episode reward
-
-    if done:
-        print("Reward accumulated =", env.episodeScore)
-        env.episodeScore = 0
-        state = env.reset()
+    selectedAction, actionProb = agent.work(observation, type_="selectActionMax")
+    observation, _, _, _ = env.step([selectedAction])
